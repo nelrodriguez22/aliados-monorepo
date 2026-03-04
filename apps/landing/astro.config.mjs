0c6d8892f3b7ce760bac0982a3666ae27e860cd9
Nelson Rodriguez <nelson.rodriguez@rcpo.com.ar>
2026-03-04 11:26:30 -0300

    fix: responsive y config de astro

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: ['**/*~', '**/*.swp', '**/*.swx']
+      }
+    }
   },
 });
